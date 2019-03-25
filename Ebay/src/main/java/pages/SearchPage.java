1317731da020276a020f5f671c2bd4b9fca64c79
ptbhowmik <ptbhowmik@gmail.com>
2019-03-25 13:14:39 -0400

    extent reporting

--- Ebay/src/main/java/pages/SearchPage.java
+++ Ebay/src/main/java/pages/SearchPage.java
@@ -1,16 +1,19 @@
 package pages;
+import base.CommonAPI;
 import datasource.DataBase;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.testng.Assert;
+import reporting.TestLogger;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-public class SearchPage {
+public class SearchPage extends CommonAPI {
 
     DataBase dataBase = new DataBase();
 
@@ -21,28 +24,39 @@ public class SearchPage {
     public static WebElement submitButtonWebElement;
 
     public WebElement getSearchInputWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         return searchInputWebElement;
     }
     public WebElement getSubmitButtonWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         return submitButtonWebElement;
     }
 
     public void searchFor(String value){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName())+ " "+ value);
         getSearchInputWebElement().sendKeys(value);
     }
     public void submitSearchButton(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         getSubmitButtonWebElement().click();
     }
     public void clearInput(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         getSearchInputWebElement().clear();
     }
     public void searchItemsAndSubmitButton()throws Exception, IOException, SQLException, ClassNotFoundException{
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         List<String> list = dataBase.getItemsListFromDB();
         for(int i=0; i<list.size(); i++) {
             searchFor(list.get(i));
             submitSearchButton();
+            itemFoundOnSearch(list.get(i));
             clearInput();
         }
+    }
+    public void itemFoundOnSearch(String value){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName())+" "+ value);
+        Assert.assertEquals(value,value);
     }
 
     public void searchItemsAndSubmitButton(WebDriver driver1)throws Exception, IOException, SQLException, ClassNotFoundException{
@@ -54,6 +68,16 @@ public class SearchPage {
         }
     }
 
+    public void searchItemsAndSubmitButtonFromExcelFile()throws Exception, IOException, SQLException, ClassNotFoundException  {
+        // ToDo
+        //Read data from Excel file using Apache POI
+        List<String> list = null;
+        for(int i=0; i<list.size(); i++) {
+            searchFor(list.get(i));
+            submitSearchButton();
+            clearInput();
+        }
+    }
 
     public WebElement getSearchInputField() {
         return searchInputWebElement;
@@ -68,6 +92,7 @@ public class SearchPage {
         for(String st: itemList) {
             getSearchInputField().sendKeys(st, Keys.ENTER);
             getSearchInputField().clear();
+            //validate items
         }
     }
 
