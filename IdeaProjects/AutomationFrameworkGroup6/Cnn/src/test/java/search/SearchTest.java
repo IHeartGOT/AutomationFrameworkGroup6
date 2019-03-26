f76d1e4665d591d227e8534a118005ea37bf87d5
alexkarmokar <ujjalkarmokar>
2019-03-26 18:00:34 -0400

    Page Factory

--- IdeaProjects/AutomationFrameworkGroup6/Cnn/src/test/java/search/SearchTest.java
+++ IdeaProjects/AutomationFrameworkGroup6/Cnn/src/test/java/search/SearchTest.java
@@ -1,23 +1,21 @@
 package search;
 
 import base.CommonAPI;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class SearchTest extends CommonAPI {
 
-    @Test
-    public void search () {
-        driver.findElement(By.id("search-input-field")).sendKeys("news", Keys.ENTER);
 
-    }
-    @Test
-    public void typeOncss() {
-        driver.findElement(By.cssSelector("#search-input-field")).sendKeys("news", Keys.ENTER);
-
+        @Test
+        public void search()throws Exception, IOException, SQLException, ClassNotFoundException{
+            SearchPage searchPage = PageFactory.initElements(driver, SearchPage.class);
+            searchPage.searchItemsAndSubmitButton();
+        }
     }
 
 
 
-}
